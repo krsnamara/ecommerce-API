983166ca68be6e8a0afbc1f87709a232ae3342ed
krsnamara <martinj.fitzpatrick@gmail.com>
2023-03-21 14:20:58 -0400

    basic structure for routes files

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,19 @@
 const router = require("express").Router();
 
-router.get("/usertest", (req,res)=>{
-    res.send("user test is successful");
-});
 
-router.post("/userposttest", (req,res)=>{
-    const username = req.body.username;
-    res.send("your username is: "+ username);
-})
+
+
+
 
 module.exports = router
+
+
+// tests
+// router.get("/usertest", (req,res)=>{
+//     res.send("user test is successful");
+// });
+
+// router.post("/userposttest", (req,res)=>{
+//     const username = req.body.username;
+//     res.send("your username is: "+ username);
+// })
