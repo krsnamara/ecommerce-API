5ccf968fa171f8c2df329850c04d1e4505553b17
krsnamara <martinj.fitzpatrick@gmail.com>
2023-03-21 13:46:11 -0400

    framing out express backend with mongoDB connection

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("hello");
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3000;
+
+mongoose.connect(process.env.DATABASE_URI).then(()=>console.log("DBConnection Successful!"))
+.catch((err)=> {
+    console.log(err);
+});
+
+app.get("/api/test", ()=>{
+    console.log("Test is successful");
+})
+
+app.listen(PORT, () => console.log('express is listening on:', PORT));
+// app.listen(3000, () => console.log("Backend Server Is Running"));
