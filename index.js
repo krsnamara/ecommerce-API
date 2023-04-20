cbb6081816766b61adca2666dcf74d7369705ad1
krsnamara <martinj.fitzpatrick@gmail.com>
2023-04-19 23:10:24 -0400

    adding cart router and routes for user crud, admin get all

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/user");
 const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
 
 const PORT = process.env.PORT || 3000;
 
@@ -22,6 +23,7 @@ app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
+app.use("/api/cart", carttRoute);
 
 app.listen(PORT, () => console.log('express is listening on:', PORT));
 // app.listen(3000, () => console.log("Backend Server Is Running"));
--- routes/product.js
+++ routes/product.js
@@ -1,5 +1,5 @@
 const Product = require("../models/Product");
-const { verifyToken, verifyTokenAndAuthorization, verifyTokenAndAdmin } = require("./verifyToken");
+const { verifyTokenAndAdmin } = require("./verifyToken");
 const router = require("express").Router();
 
 // CREATE PRODUCT //
@@ -60,7 +60,7 @@ router.get("/", async (req, res) => {
       let products;
 
       if (qNew){
-        products = await Product.find().sort({createdAt: -1}).limit(5)
+        products = await Product.find().sort({ createdAt: -1 }).limit(5);
       } else if (qCategory){
         products = await Product.find({
           categories:{
@@ -70,36 +70,7 @@ router.get("/", async (req, res) => {
       } else {
         products = await Product.find();
       }
-
-
       res.status(200).json(products);
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
-  
-  //GET PRODUCT STATS //
-  
-  router.get("/stats", verifyTokenAndAdmin, async (req, res) => {
-    const date = new Date();
-    const lastYear = new Date(date.setFullYear(date.getFullYear() - 1));
-  
-    try {
-      const data = await User.aggregate([
-        { $match: { createdAt: { $gte: lastYear } } },
-        {
-          $project: {
-            month: { $month: "$createdAt" },
-          },
-        },
-        {
-          $group: {
-            _id: "$month",
-            total: { $sum: 1 },
-          },
-        },
-      ]);
-      res.status(200).json(data)
     } catch (err) {
       res.status(500).json(err);
     }
